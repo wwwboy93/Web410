6092fa59a895a854676b7c92a7dcad3b5dc049ce
Reggie-Zhou <reggiezj@gmail.com>
2016-11-03 18:27:34 -0400

    add py_path.py to change python interpreter path

---
+++ www/cgi-bin/py_path.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+'''
+change python interpreter path for all python files in this directory.
+
+The method to do the replacement is very shitty; but it works.
+'''
+
+import os
+import sys
+import re
+
+py_path = ""
+if sys.argv[1] == "mac":
+    py_path = "#!/usr/bin/python\n"
+else:
+    py_path = "#!C:\Python27\python\n"
+
+for file_name in os.listdir():
+    if re.search("\.py", file_name) is not None:
+        # find a .py file
+        target_file = open(file_name, "r")
+        file_content = target_file.readlines()
+        target_file.close()
+        file_content[0] = py_path
+        target_file = open(file_name, "w")
+        for line in file_content:
+            target_file.write(line)
+        target_file.close()
+    
